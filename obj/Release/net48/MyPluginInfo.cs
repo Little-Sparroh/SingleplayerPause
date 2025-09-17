6b42359f1962d332ab264cfc4f93a3623d4c68d4
Little-Sparroh <wingedoracle@tutanota.com>
2025-09-17 19:40:12 +0000

    Delete obj/Release/net48 directory

--- obj/Release/net48/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace SingleplayerPause
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "SingleplayerPause";
-        public const string PLUGIN_NAME = "SingleplayerPause";
-        public const string PLUGIN_VERSION = "..";
-    }
-}
